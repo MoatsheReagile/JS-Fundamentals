c68b6f68eef1fda2d0da98e5d4f388f699dad8e5
unknown <moatshereagile@gmail.com>
2025-11-07 20:41:19 +0200

    Add script that prints 3 lines using loop and array

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,11 +1,4 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-let output = "";
-for (let i = 0; i < lines.length; i++) {
-  output += lines[i];
-  if (i < lines.length - 1) {
-    output += "\n";
-  }
-}
-
-console.log(output);
+#!/usr/bin/node
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = languages.join('\n');
+console.log(result);
