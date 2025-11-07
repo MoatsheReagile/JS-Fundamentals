e9cb38126aae8511ebf0333bcb143d8c41c411c0
unknown <moatshereagile@gmail.com>
2025-11-07 12:36:27 +0200

    Add script printing languages using a while loop and one console.log

---
+++ 0x12/6-multi_languages_loop.js
@@ -0,0 +1,14 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let i = 0;
+let output = "";
+
+while (i < lines.length) {
+  output += lines[i];
+  if (i !== lines.length - 1) {
+    output += "\n";
+  }
+  i++;
+}
+
+console.log(output);
